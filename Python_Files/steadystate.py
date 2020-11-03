fdee1ab1c7a82e4cccc1380489f8ff22174b18a9
YunHsiuLu <yulu@YunHsiuLudeMacBook-Pro.local>
2020-11-03 17:40:03 +0800

    20201103 upload

--- Python_Files/steadystate.py
+++ Python_Files/steadystate.py
@@ -1,26 +1,40 @@
 def CalMatrix(mat):
     n = len(mat)
-    new_mat = [[0 for _ in range(n)] for i in range(n)]
-    d1 = mat[0][0] - mat[1][0]
-    d2 = mat[0][1] - mat[1][1]
+    NewMat = [[0 for _ in range(n)] for i in range(n)]
+    d = [1 for _ in range(n)]
     power = 1
-    while (abs(d1) >= 0.005 or abs(d2) >= 0.005):
+    print(mat)
+    while (True):
+        check = 0
+        for ind in range(n):
+            if (d[ind] <= 0.00001):
+                check += 1
+        if check == n:
+            break
         for i in range(n):
             for j in range(n):
                 for c in range(n):
-                    new_mat[i][j] += mat[i][c]*mat[c][j]
-        mat = new_mat
-        new_mat = [[0 for _ in range(n)] for i in range(n)]
+                    NewMat[i][j] += mat[i][c]*mat[c][j]
+        for ind in range(n):
+            d[ind] = abs(NewMat[0][ind] - mat[0][ind])
+        mat = NewMat
+        NewMat = [[0 for _ in range(n)] for i in range(n)]
         power += 1
-        d1 = mat[0][0] - mat[1][0]
-        d2 = mat[0][1] - mat[1][1]
         print("power: %d" % (power))
-        print(mat)
     return mat
 
-testMatrix = [[0.95, 0.05], [0.6, 0.4]]
-CalMatrix(testMatrix)
-
+testMatrix = [
+    [0.8, 0.2, 0.0, 0.0],
+    [0.4, 0.0, 0.6, 0.0],
+    [0.4, 0.0, 0.0, 0.6],
+    [1.0, 0.0, 0.0, 0.0]
+]
+testMatrix = CalMatrix(testMatrix)
+n = len(testMatrix)
+for i in range(n):
+    for j in range(n):
+        print("%.2f" % (testMatrix[i][j]), end=" ")
+    print()
 #(0,0) = (0,0)*(0,0) + (0,1)*(1,0)
 #(0,1) = (0,0)*(0,1) + (0,1)*(1,1)
 #(1,0) = (1,0)*(0,0) + (1,1)*(1,0)
