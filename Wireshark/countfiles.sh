da3b45acfbf232ad7b7f8fecc02fc9055f53e3c2
YunHsiuLu <yulu@YunHsiuLudeMacBook-Pro.local>
2020-12-13 17:31:18 +0800

    20201213 upload

--- Wireshark/countfiles.sh
+++ Wireshark/countfiles.sh
@@ -1,3 +1,4 @@
+sleep_sec=$(($1 + 2))
 for i in {1..$2};
 do
     sleep $1
@@ -5,7 +6,6 @@ do
         arr=(*.pcap)
         echo "Files done: $i"
     cd ../
-    #echo "${arr[$i]}"
     source pcap2csv.sh ${arr[$i]}
 done
 
--- Wireshark/pcap2csv.sh
+++ Wireshark/pcap2csv.sh
@@ -2,5 +2,4 @@ filename=$1
 echo $filename
 str="test${filename:8:5}.csv"
 echo $str
-#tshark -r $i -T fields -e frame.number -e eth.src -e eth.dst -e ip.src -e ip.dst -e frame.len -E header=y -E separator=, > "data_csv"/$str
-
+tshark -r "data_test"/$filename -T fields -e frame.number -e eth.src -e eth.dst -e ip.src -e ip.dst -e frame.len -E header=y -E separator=, > "data_testcsv"/$str
