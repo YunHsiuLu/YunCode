b1f37ea7f7cd1cb1e50d34d9c35dc29250fbe115
YunHsiuLu <cow3690m@gmail.com>
2024-02-11 09:33:45 +0000

    a445.cpp

--- C_Files/ZeroJudge/a445.cpp
+++ C_Files/ZeroJudge/a445.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-struct node {
-    vector<int> relation;
-    int val;
-    bool visited;
-};
+const int SIZE=10001;
+int parent[SIZE], rk[SIZE];
 
-const int SIZE = 10000;
-node PERSON[SIZE+1];
-bool ans;
-
-void dfs(node &tmp1, node &tmp2) {
-    //cout << tmp1.val << " " << tmp2.val << endl;
-    tmp1.visited = true;
-    if (tmp1.val == tmp2.val) ans = true;
-    for (int i = 0; i < tmp1.relation.size(); i++) {
-        if (!PERSON[tmp1.relation[i]].visited)
-            dfs(PERSON[tmp1.relation[i]], tmp2);
-    }
+int find_parent(int x) {
+	if (parent[x] == x) return x;
+	else return find_parent(parent[x]);
 }
 
 int main() {
-    cin.sync_with_stdio(0);
-    cin.tie(0);
-    int N, M, Q;
-    cin >> N >> M >> Q;
-    for (int i = 0; i < M; i++) {
-        int first, second;
-        cin >> first >> second;
-        cout << first << " " << second;
-        if (!PERSON[first].val) PERSON[first].val = first;
-        if (!PERSON[second].val) PERSON[second].val = second;
-        PERSON[first].relation.push_back(second);
-        PERSON[second].relation.push_back(first);
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	int M, N, Q;
+	cin >> N >> M >> Q;
+	for (int i = 1; i <= N; i++) rk[i] = 1;
+	
+	int c = 0;
+	while (c < M) {
+		int first, second;
+		cin >> first >> second;
+		if (!parent[first]) parent[first] = first;
+		if (!parent[second]) parent[second] = second;
+		cout << parent[first] << endl;
+		if (rk[find_parent(first)] >= rk[find_parent(second)]) {
+			parent[second] = first;
+			rk[first] += rk[second];
+		}
+		else {
+			parent[first] = second;
+			rk[second] += rk[first];
+		}
+		c++;
+	}
+	c = 0;
+	while (c < Q) {
+		int first, second;
+		cin >> first >> second;
+		if (find_parent(first) == find_parent(second)) cout << ":)" << endl;
+		else cout << ":(" << endl;
+		c++;
+	}
+	return 0;
+}
+
+PERSON[second].relation.push_back(first);
     }
     for (int i = 0; i < Q; i++) {
         for (int j = 0; j < N; j++) PERSON[j].visited = false;
