b7ca38ecd2c0bfe36d2761007f9cb3abe1a10690
YunHsiuLu <cow3690m@gmail.com>
2024-07-30 01:16:13 +0800

    upload a445

--- C_Files/ZeroJudge/a445.cpp
+++ C_Files/ZeroJudge/a445.cpp
@@ -1,51 +1,44 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-struct node {
-    vector<int> relation;
-    int val;
-    bool visited;
-};
+int p[10001], r[10001];
 
-const int SIZE = 10000;
-node PERSON[SIZE+1];
-bool ans;
+int Find(int x) {
+    return (p[x] == -1 ? x : p[x] = Find(p[x]));
+}
 
-void dfs(node &tmp1, node &tmp2) {
-    //cout << tmp1.val << " " << tmp2.val << endl;
-    tmp1.visited = true;
-    if (tmp1.val == tmp2.val) ans = true;
-    for (int i = 0; i < tmp1.relation.size(); i++) {
-        if (!PERSON[tmp1.relation[i]].visited)
-            dfs(PERSON[tmp1.relation[i]], tmp2);
+void Joint(int x, int y) {
+    x = Find(x), y = Find(y);
+    if (x == y) return;
+
+    if (r[x] > r[y]) {
+        r[x] += r[y];
+        p[y] = x;
+    }
+    else {
+        r[y] += r[x];
+        p[x] = y;
     }
 }
 
 int main() {
-    cin.sync_with_stdio(0);
-    cin.tie(0);
-    int N, M, Q;
-    cin >> N >> M >> Q;
-    for (int i = 0; i < M; i++) {
-        int first, second;
-        cin >> first >> second;
-        cout << first << " " << second;
-        if (!PERSON[first].val) PERSON[first].val = first;
-        if (!PERSON[second].val) PERSON[second].val = second;
-        PERSON[first].relation.push_back(second);
-        PERSON[second].relation.push_back(first);
-    }
-    for (int i = 0; i < Q; i++) {
-        for (int j = 0; j < N; j++) PERSON[j].visited = false;
-        ans = false;
-        int first, second;
-        cin >> first >> second;
-        dfs(PERSON[first], PERSON[second]);
-        if (ans) cout << ":)\n";
-        else cout << ":(\n";
+    int n, m, q, x, y;
+    cin >> n >> m >> q;
+    for (int i = 1; i <= n; i++) {
+        p[i] = -1;
+        r[i] = 1;
     }
 
-	return 0;
+    while (m--) {
+        cin >> x >> y;
+        Joint(x, y);
+    }
+
+    while (q--) {
+        cin >> x >> y;
+        if (Find(x) == Find(y)) cout << ":)" << endl;
+        else cout << ":(" << endl;
+    }
+    return 0;
 }
 
